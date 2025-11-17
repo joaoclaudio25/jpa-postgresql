96a575631dc25a78774312b0f0eeb01be487dc8e
JC <joao.claudio25@gmail.com>
2025-11-17 15:21:20 -0300

    Ajustando api-key no código

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -19,7 +19,7 @@ public class Principal {
     private ConsumoApi consumo = new ConsumoApi();
     private ConverteDados conversor = new ConverteDados();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
-    private final String API_KEY = "&apikey=6585022c";
+    private final String API_KEY = "&apikey=" + System.getenv("OMDB_APIKEY");  // sem '+', sem ${}
     private List<DadosSerie> dadosSeries = new ArrayList<>();
 
     private SerieRepository repositorio;
